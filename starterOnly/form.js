3b60430c98083aa97418124435e09b8fda2244d7
Angelina <50414739+angelinalavoryk@users.noreply.github.com>
2023-02-26 21:53:19 +0100

    ajout de vérification de la date d'anniversaire + quantity + conditions

---
+++ starterOnly/form.js
@@ -0,0 +1,185 @@
+// element correspondant au bouton 
+const formValid = document.querySelector("#btn-submit");
+
+
+// const confirmationValidation = document.querySelector("#confirm-modal");
+
+// element pour l'envoi du formulaire 
+const form = document.querySelector('form[name="reserve"]');
+
+
+// Envoi du formulaire d'inscription
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    validate();
+  });
+
+// Fonction qui valide le champ de prénom
+function validateFirstName() {
+    const errorFirstName = document.querySelector("#errorFirst");
+    const firstName = document.querySelector("#first");
+    if (firstName.value.toString().trim().length >= 2) {
+        errorFirstName.style.display = 'none';
+        firstName.style.border = 'solid #279e7a 3px';
+        return true; 
+    } else {
+        errorFirstName.style.display = "inline";
+        errorFirstName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ prénom.";
+        errorFirstName.style.color = 'red';
+        errorFirstName.style.fontSize = '0.8rem';
+        errorFirstName.style.marginTop = '10px';
+        firstName.style.border = 'solid red 2px';
+        return false;
+    };
+  }
+  
+// Fonction qui valide le champ de nom de famille
+  function validateLastName() {
+    const errorLastName = document.querySelector("#errorLast");
+    const lastName = document.querySelector("#last");
+    if (lastName.value.toString().trim().length >= 2) {
+        errorLastName.style.display = 'none';
+        lastName.style.border = 'solid #279e7a 3px';
+        return true;
+    } else {
+        errorLastName.style.display = 'inline';
+        errorLastName.innerText = "Veuillez entrer 2 caractères ou plus pour le champ nom.";
+        errorLastName.style.color = 'red';
+        errorLastName.style.fontSize = '0.8rem';
+        errorLastName.style.marginTop = '10px';
+        lastName.style.border = 'solid red 2px';
+        return false;
+    };
+  }
+  
+// Fonction qui valide le champ email
+  function validateEmail() {
+    const errorEmail = document.querySelector("#errorEmail");
+    const email = document.querySelector("#email");
+    if (/[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email.value)) {
+        errorEmail.style.display = 'none';
+        email.style.border = 'solid #279e7a 3px';
+        return true;
+    } else {
+        errorEmail.style.display = "inline";
+        errorEmail.innerText = "L'adresse email est incomplete. Veuillez entrer la bonne adresse.";
+        errorEmail.style.color = 'red';
+        errorEmail.style.fontSize = '0.8rem';
+        errorEmail.style.marginTop = '10px';
+        email.style.border = 'solid red 2px';
+        return false;
+    };
+  }
+
+  // Fonction qui valide le champ birthdate
+  function validateBirthdate() {
+    const errorBirthDate = document.querySelector("#errorBirthdate");
+    const birthdate = document.querySelector("#birthdate");
+    const birthdateRegex = /^(19|20)\d{2}-(0\d|1[0-2])-([0-2]\d|3[0-1])$/;
+    if (birthdateRegex.test(birthdate.value)) {
+        errorBirthDate.style.display = 'none';
+        birthdate.style.border = 'solid #279e7a 3px';
+        return true;
+      } else {
+        errorBirthDate.style.display = "inline";
+        errorBirthDate.innerText = "Veuillez indiquer votre date de naissance.";
+        errorBirthDate.style.color = 'red';
+        errorBirthDate.style.fontSize = '0.8rem';
+        errorBirthDate.style.marginTop = '10px';
+        birthdate.style.border = 'solid red 2px';
+        return false;
+      };
+  }
+
+// Fonction qui valide le champ de concours
+function validateQuantity() {
+    const errorQuantity = document.querySelector("#errorQuantity");
+    const quantity = document.querySelector("#quantity");
+    const value = parseInt(quantity.value);
+    if (value <= 99){
+        errorQuantity.style.display = 'none';
+        quantity.style.border = 'solid #279e7a 3px';
+        return true;
+    }
+    else{
+        errorQuantity.style.display = "inline";
+        errorQuantity.innerText = "Pour le nombre de concours, une valeur numérique entre 0 et 99 doit etre saisie.";
+        errorQuantity.style.color = 'red';
+        errorQuantity.style.fontSize = '0.8rem';
+        errorQuantity.style.marginTop = '10px';
+        quantity.style.border = 'solid red 2px';
+        return false;
+    };
+}
+
+// function validateLocation(){
+//     const errorLocation = document.querySelector("#errorLocation");
+//     const location = document.getElementsByClassName(".location");
+// }
+
+function validateConditions(){
+    const errorConditions = document.querySelector("#errorConditions");
+    const conditions = document.querySelector("#checkbox1");
+    if (conditions.checked == true){
+        errorConditions.style.display = 'none';
+        return true;
+    }else {
+        errorConditions.style.display = "inline";
+        errorConditions.innerText = "Vous devez accepter les termes et conditions.";
+        errorConditions.style.color = 'red';
+        errorConditions.style.fontSize = '0.8rem';
+        errorConditions.style.marginTop = '10px';
+        return false;
+    }; 
+}
+
+
+
+//   function validateBirthdate() {
+//     if (!birthDate.value.match(/^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/)) {
+        
+
+//         errorBirthDate.style.display = 'none';
+//         birthDate.style.border = 'solid #279e7a 3px';
+//         console.log("2");
+//     } else {
+//         errorBirthDate.style.display = "inline";
+//         errorBirthDate.innerText = "Veuillez indiquer votre date de naissance.";
+//         errorBirthDate.style.color = 'red';
+//         errorBirthDate.style.fontSize = '0.8rem';
+//         errorBirthDate.style.marginTop = '10px';
+//         birthDate.style.border = 'solid red 2px';
+//         console.log("1");
+//     };
+//   }
+
+
+
+
+
+
+
+  
+
+  function validate() {
+      const formValidate = [];
+    
+      formValidate.push(validateFirstName());
+      formValidate.push(validateLastName());
+      formValidate.push(validateEmail());
+      formValidate.push(validateBirthdate());
+      formValidate.push(validateQuantity());
+      formValidate.push(validateConditions());
+
+      if (!formValidate.includes(false)) {
+            form.style.display = 'none';
+      };
+    }
+
+
+
+
+const confirmationCloseBtn = document.querySelector("#btn-closed"); // bouton "fermer"
+document.querySelector("#btn-closed").addEventListener("click", closeModal);// Fermer le formulaire avec le message de validation 
+
+
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,15 +9,45 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelectorAll(".modal-btn"); //faire apparaitre le formulaire
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close"); //fermer le formulaire
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+// Close modal event
+closeBtn.addEventListener('click', closeModal);
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// Close modal form
+function closeModal(){
+  modalbg.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
